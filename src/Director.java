d6e085ebd179c2ede3bf82cc2cbd7cdf73a329fa
Jakub <dimetrodon@wp.pl>
2017-11-22 00:11:36 +0100

    company commit

---
+++ src/Director.java
@@ -0,0 +1,11 @@
+
+public class Director extends Manager implements Orders {
+
+	public Director(String firstName, String secondName, String jobTitle, int yearOfBirth, char gender, double salary, int percentageHappiness) {
+		super(firstName, secondName, jobTitle, yearOfBirth, gender, salary, percentageHappiness);
+		
+	}
+	
+	
+
+}
