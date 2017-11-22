b043cff24cbd1eb66ba7ad518ea3f10c3386b1b1
Kaminski Jakub <dimetrodon@wp.pl>
2017-11-23 00:47:55 +0100

    commit

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
-
 public interface Tasks {
 
 	void presentYourself(String firstName, String secondName, String jobTitle); 
 	void sayHello();
 	// void workOnWeekends();
 	final static int CHRISTMAS_BONUS = 300;
-
-	
 	
 
 }
-=======
-
-public interface Tasks {
-
-	void presentYourself(String firstName, String secondName, String jobTitle); 
-	void sayHello();
-	// void workOnWeekends();
-	final static int CHRISTMAS_BONUS = 300;
-
-	
-	
-
-}
->>>>>>> 681d41923e9ff554cb1be5de8e333a37de1a8483
